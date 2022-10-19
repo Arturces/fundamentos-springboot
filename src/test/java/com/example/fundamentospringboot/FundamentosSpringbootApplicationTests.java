c59fc57028b0b6e15770afb6e0a1144cef050f15
Artur César <arturcesar57@gmail.com>
2022-10-18 21:27:19 -0300

    Projeto Criado

---
+++ src/test/java/com/example/fundamentospringboot/FundamentosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fundamentospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
