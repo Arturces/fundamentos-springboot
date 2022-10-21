1d888d6aa14167612895c05cc32f389fe7ab1233
Artur César <arturcesar57@gmail.com>
2022-10-21 20:57:51 -0300

    Add valores da URL para  WebService
    @PathVariable - passagem de parametros dentro da url

--- src/main/java/com/example/fundamentospringboot/controllers/ClienteController.java
+++ src/main/java/com/example/fundamentospringboot/controllers/ClienteController.java
@@ -1,9 +1,7 @@
 package com.example.fundamentospringboot.controllers;
 
 import com.example.fundamentospringboot.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/clientes")
@@ -12,5 +10,10 @@ public class ClienteController {
     @GetMapping(path = "/qualquer")
     public Cliente obterCliente(){
         return new Cliente(28L, "Artur", "056.501.405-61");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente ObterCLientePorId1(@PathVariable  Long id){
+        return new Cliente(id, "Artur", "056,501,405-61");
     }
 }
