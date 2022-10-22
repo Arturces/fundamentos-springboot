5813dbe73784b1bd34b5d18b072bafcbec726380
Artur César <arturcesar57@gmail.com>
2022-10-22 14:00:29 -0300

    Add metodode  Nome Contains na busca por Produto

--- src/main/java/com/example/fundamentospringboot/model/repository/ProdutoRepository.java
+++ src/main/java/com/example/fundamentospringboot/model/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.example.fundamentospringboot.model.repository;
 import com.example.fundamentospringboot.model.entidades.Produto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends CrudRepository<Produto, Integer > {
+
+    public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 
 }
